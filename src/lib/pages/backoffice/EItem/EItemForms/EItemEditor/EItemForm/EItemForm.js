183199065aa188119a2f022811e223184c6b5752
jrcastro2 <jrcastro9515@gmail.com>
2020-08-14 14:37:22 +0200

    eitemForms: Reorganize forms
    * closes #165

--- src/lib/pages/backoffice/EItem/EItemForms/EItemEditor/EItemForm/EItemForm.js
+++ src/lib/pages/backoffice/EItem/EItemForms/EItemEditor/EItemForm/EItemForm.js
@@ -1,6 +1,6 @@
 import { documentApi } from '@api/documents';
-import { invenioConfig } from '@config';
 import { eItemApi } from '@api/eitems';
+import { invenioConfig } from '@config';
 import { UrlsField } from '@forms/components/UrlsField';
 import { BaseForm } from '@forms/core/BaseForm';
 import { BooleanField } from '@forms/core/BooleanField';
@@ -8,13 +8,14 @@ import { SelectorField } from '@forms/core/SelectorField';
 import { TextField } from '@forms/core/TextField';
 import { goTo } from '@history';
 import { serializeDocument } from '@modules/ESSelector/serializer';
+import { Identifiers } from '@pages/backoffice/Document/DocumentForms/DocumentEditor/DocumentForm/components';
 import { BackOfficeRoutes } from '@routes/urls';
 import { getIn } from 'formik';
 import pick from 'lodash/pick';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Grid, Header, Segment } from 'semantic-ui-react';
 import eitemSubmitSerializer from './eitemSubmitSerializer';
-import { Identifiers } from '@pages/backoffice/Document/DocumentForms/DocumentEditor/DocumentForm/components';
 
 export class EItemForm extends Component {
   prepareData = data => {
@@ -58,24 +59,73 @@ export class EItemForm extends Component {
         pid={pid ? pid : undefined}
         submitSerializer={eitemSubmitSerializer}
       >
-        <SelectorField
-          required
-          emptyHeader="No document selected"
-          emptyDescription="Please select a document."
-          fieldPath="document"
-          errorPath="document_pid"
-          label="Document"
-          placeholder="Search for a document..."
-          query={documentApi.list}
-          serializer={serializeDocument}
-        />
-        <TextField label="Description" fieldPath="description" rows={5} />
-        <BooleanField toggle label="Open access" fieldPath="open_access" />
-        <UrlsField />
-        <TextField label="Internal notes" fieldPath="internal_notes" rows={5} />
-        <Identifiers
-          scheme={invenioConfig.VOCABULARIES.document.identifier.scheme}
-        />
+        <Header as="h3" attached="top">
+          Basic information
+        </Header>
+        <Segment attached>
+          <Grid stretched>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <SelectorField
+                  required
+                  emptyHeader="No document selected"
+                  emptyDescription="Please select a document."
+                  fieldPath="document"
+                  errorPath="document_pid"
+                  label="Document"
+                  placeholder="Search for a document..."
+                  query={documentApi.list}
+                  serializer={serializeDocument}
+                />
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <BooleanField
+                  toggle
+                  label="Open access"
+                  fieldPath="open_access"
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+
+          <Grid columns="equal">
+            <Grid.Row>
+              <Grid.Column>
+                <TextField
+                  label="Description"
+                  fieldPath="description"
+                  rows={5}
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <TextField
+                  label="Internal notes"
+                  fieldPath="internal_notes"
+                  rows={5}
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+
+          <Grid columns="equal">
+            <Grid.Row>
+              <Grid.Column>
+                <Segment>
+                  <UrlsField />
+                </Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment>
+                  <Identifiers
+                    scheme={
+                      invenioConfig.VOCABULARIES.document.identifier.scheme
+                    }
+                  />
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
       </BaseForm>
     );
   }
