5b9e4c7cb7e3f566c5f5ed543b5bc4242bfad8f3
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2020-10-06 11:40:57 +0200

    backoffice: fix open access label

--- src/lib/components/backoffice/OpenAccessLabel/OpenAccessLabel.js
+++ src/lib/components/backoffice/OpenAccessLabel/OpenAccessLabel.js
@@ -8,17 +8,15 @@ import { Icon, Label } from 'semantic-ui-react';
 export default class OpenAccessLabel extends Component {
   render() {
     const { openAccess, size } = this.props;
-    return (
-      openAccess && (
-        <>
-          <Label size={size} color="green">
-            <Icon name="lock open" />
-            Open access
-          </Label>
-          <br />
-        </>
-      )
-    );
+    return openAccess ? (
+      <>
+        <Label size={size} color="green">
+          <Icon name="lock open" />
+          Open access
+        </Label>
+        <br />
+      </>
+    ) : null;
   }
 }
 
