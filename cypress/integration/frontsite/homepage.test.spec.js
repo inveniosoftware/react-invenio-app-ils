4c335d00390e254e989cc4e9e3a86bb33709f84f
KonstantinaStoikou <konstantinastoikou1@gmail.com>
2020-10-22 14:10:17 +0200

    changes

--- cypress/integration/frontsite/homepage.test.spec.js
+++ cypress/integration/frontsite/homepage.test.spec.js
@@ -18,19 +18,49 @@ describe('frontsite homepage', () => {
     cy.url().should('contain', 'ipsum');
   });
 
-  it('should be able to find the recent literature', () => {
+  it('should be able to find recent books', () => {
     cy.visit('/');
 
     cy.contains('Recent books').click();
 
     cy.url().should('contain', Cypress.config().baseUrl + '/search');
+    cy.url().should('include', 'sort=created');
+    cy.url().should('include', 'order=desc');
   });
 
-  it('should have a link to the library schedule', () => {
+  it('should be able to find most loaned books', () => {
     cy.visit('/');
 
-    cy.contains('Opening hours').click();
+    cy.contains('Most loaned books').click();
 
-    cy.contains('h2', 'Opening hours');
+    cy.url().should('contain', Cypress.config().baseUrl + '/search');
+    cy.url().should('include', 'sort=mostloaned');
+    cy.url().should('include', 'order=desc');
+  });
+
+  it('should be able to find new ebooks', () => {
+    cy.visit('/');
+
+    cy.contains('New e-books').click();
+
+    cy.url().should('contain', Cypress.config().baseUrl + '/search');
+    cy.url().should('include', 'sort=created');
+    cy.url().should('include', 'order=desc');
+    cy.url().should('include', 'doctype%3ABOOK');
+    cy.url().should('include', 'medium%3AELECTRONIC_VERSION');
+  });
+
+  it('should be able to click on first VIEW ALL', () => {
+    cy.visit('/');
+    cy.contains('VIEW ALL').click();
+    cy.url().should('contain', Cypress.config().baseUrl + '/search');
+  });
+
+  it('should be able to go to details of book', () => {
+    cy.visit('/');
+    cy.get('.fs-book-card')
+      .eq(0)
+      .click();
+    cy.url().should('contain', Cypress.config().baseUrl + '/literature');
   });
 });
