3acf5c8337a31538446f3235518140a5856e024e
Konstantina Stoikou <konstantinastoikou1@gmail.com>
2020-11-25 14:21:43 +0100

    FrontsiteLoanRequest: small fixes

--- src/lib/pages/frontsite/Documents/DocumentDetails/DocumentCirculation/LoanAvailability.js
+++ src/lib/pages/frontsite/Documents/DocumentDetails/DocumentCirculation/LoanAvailability.js
@@ -2,54 +2,63 @@ import { DateTime } from 'luxon';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { List, Popup } from 'semantic-ui-react';
+import Overridable from 'react-overridable';
 
 export class LoanAvailability extends Component {
   render() {
     const { circulation } = this.props;
     if (circulation.available_items_for_loan_count > 0) {
       return (
-        <List.Item>
-          <Popup
-            content="Calculated based on current library stock"
-            trigger={<List.Icon name="info circle" />}
-          />
-          <List.Content
-            className={
-              circulation.available_items_for_loan_count > 0
-                ? 'text-success'
-                : 'text-danger'
-            }
-          >
-            Available for loan <span className="success">now</span>
-          </List.Content>
-        </List.Item>
+        <Overridable id="LoanAvailability.AvailableNow">
+          <List.Item>
+            <Popup
+              content="Calculated based on current library stock"
+              trigger={<List.Icon name="info circle" />}
+            />
+            <List.Content
+              className={
+                circulation.available_items_for_loan_count > 0
+                  ? 'text-success'
+                  : 'text-danger'
+              }
+            >
+              Available for loan <span className="success">now</span>
+            </List.Content>
+          </List.Item>
+        </Overridable>
       );
     } else if (circulation.next_available_date) {
       return (
-        <List.Item>
-          <List.Icon name="info circle" />
-          <List.Content>
-            Available for loan from{' '}
-            <b>
-              {DateTime.fromISO(circulation.next_available_date).toLocaleString(
-                {
+        <Overridable id="LoanAvailability.AvailableFrom">
+          <List.Item>
+            <List.Icon name="info circle" />
+            <List.Content>
+              Available for loan from{' '}
+              <b>
+                {DateTime.fromISO(
+                  circulation.next_available_date
+                ).toLocaleString({
                   locale: 'en-GB',
                   month: 'long',
                   day: 'numeric',
-                }
-              )}
-            </b>
-          </List.Content>
-        </List.Item>
+                })}
+              </b>
+            </List.Content>
+          </List.Item>
+        </Overridable>
       );
     } else {
       return (
-        <List.Item>
-          <List.Icon name="info" />
-          <List.Content>
-            Long waiting queue. Contact the library for more information.
-          </List.Content>
-        </List.Item>
+        <Overridable id="LoanAvailability.NotAvailable">
+          <List.Item>
+            <List.Icon name="info" />
+            <List.Content>
+              There are no physical copies for this literature currently
+              available at the library. If you would like to loan it, please
+              place a request. The library staff will evaluate it.
+            </List.Content>
+          </List.Item>
+        </Overridable>
       );
     }
   }
@@ -58,3 +67,5 @@ export class LoanAvailability extends Component {
 LoanAvailability.propTypes = {
   circulation: PropTypes.object.isRequired,
 };
+
+export default Overridable.component('LoanAvailability', LoanAvailability);
