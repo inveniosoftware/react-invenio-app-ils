f2cd5ed9433a4dd747c75ad0311396559a6d1638
jrcastro2 <jrcastro9515@gmail.com>
2021-03-02 12:22:56 +0100

    IE: default message
    * Adds unsupported browser
      message for IE

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
+import 'react-app-polyfill/ie11'; // For IE 11 support
+import 'react-app-polyfill/stable';
 import history from '@history';
 import React from 'react';
-import 'react-app-polyfill/ie11'; // For IE 11 support
 import ReactDOM from 'react-dom';
 import { OverridableContext } from 'react-overridable';
 import { Router } from 'react-router-dom';
 import 'semantic-ui-less/semantic.less';
 import { InvenioILSApp } from './lib';
+import { IEFallback } from './lib/components/Fallbacks/IEFallback';
 
 const config = {};
 const overriddenCmps = {};
 
-ReactDOM.render(
-  <Router history={history}>
-    <OverridableContext.Provider value={overriddenCmps}>
-      <InvenioILSApp config={config} />
-    </OverridableContext.Provider>
-  </Router>,
-  document.getElementById('app')
-);
+// Checks if browser is IE (unsupported)
+const isIE = !!document.documentMode;
+
+if (!isIE) {
+  ReactDOM.render(
+    <Router history={history}>
+      <OverridableContext.Provider value={overriddenCmps}>
+        <InvenioILSApp config={config} />
+      </OverridableContext.Provider>
+    </Router>,
+    document.getElementById('app')
+  );
+} else {
+  IEFallback();
+}
