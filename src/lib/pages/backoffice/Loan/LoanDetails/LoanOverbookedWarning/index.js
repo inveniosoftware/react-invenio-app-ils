inveniosoftware/invenio-app-ils-ui

loan: show a warning in the loans of overbooked documents * closes  CERNDocumentServer/cds-ils#523

src/lib/pages/backoffice/Loan/LoanDetails/LoanOverbooked/index.js
@@ -1,0 +1,1 @@
+export { default as LoanOverbooked } from './LoanOverbooked';

inveniosoftware/react-invenio-app-ils

src/lib/pages/backoffice/Loan/LoanDetails/LoanOverbookedWarning/index.js
@@ -1,0 +1,1 @@
+export { default as LoanOverbookedWarning } from './LoanOverbookedWarning';