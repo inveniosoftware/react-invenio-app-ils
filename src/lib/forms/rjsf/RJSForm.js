287929d83056ff4274b08905237f2d8a533c8efe
nico <nicolas.rodriguezcuellar@student.hu.nl>
2021-12-06 10:08:06 +0100

    RSJF: Fix bug on enter press
    Now disables the onEnter event on RJSF inputs because it was causing some odd behaviour with empty fields spawning out of nowhere when pressing enter.

--- src/lib/forms/rjsf/RJSForm.js
+++ src/lib/forms/rjsf/RJSForm.js
@@ -56,6 +56,25 @@ export class RJSForm extends Component {
     };
   }
 
+  componentDidMount() {
+    const inputs = document.getElementsByTagName('input');
+
+    this.ignoreEnterEvent(inputs);
+  }
+
+  ignoreEnterEvent = (inputElements) => {
+    /* For unknown reasons empty fields are spawned when pressing
+    the 'enter' key inside a RJSF input element so we have to manually prevent it*/
+
+    for (const input of inputElements) {
+      const isRJSFInput = input.id.includes('root');
+      if (isRJSFInput) {
+        input.onkeydown = (event) =>
+          event.key === 'Enter' ? event.preventDefault() : null;
+      }
+    }
+  };
+
   getGenericError(message) {
     return { Error: { __errors: [message] } };
   }
