34fa5f35c9d5b26e49202bb5dfbfa68920778cc4
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2022-01-13 15:52:02 +0100

    series details: fix volumes query
    * closes https://github.com/CERNDocumentServer/cds-ils/issues/617

--- src/lib/api/literature/literature.test.js
+++ src/lib/api/literature/literature.test.js
@@ -3,16 +3,14 @@ import { literatureApi } from './literature';
 describe('Literature query builder tests', () => {
   it('should build the query string for literature that belong to a series', () => {
     const query = literatureApi.query().withSeriesPid('123', 'SERIAL').qs();
-    expect(query).toEqual('relations.serial.pid_value:123');
+    expect(query).toEqual('relations.serial.pid_value:"123"');
   });
 
   it('should build the query string for literature that belong to multiple series', () => {
     const query = literatureApi
       .query()
-      .withSeriesPid(['123', '567'], 'MULTIPART_MONOGRAPH')
+      .withSeriesPid('123', 'MULTIPART_MONOGRAPH')
       .qs();
-    expect(query).toEqual(
-      'relations.multipart_monograph.pid_value:(123 OR 567)'
-    );
+    expect(query).toEqual('relations.multipart_monograph.pid_value:"123"');
   });
 });
