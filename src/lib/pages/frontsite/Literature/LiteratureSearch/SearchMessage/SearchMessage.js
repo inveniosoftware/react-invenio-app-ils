2a943f69c047ef6b09ed8be0405b20e5912514cd
anikachurilova <nyuta.churilova@gmail.com>
2022-08-19 11:22:03 +0200

    simplify function call

--- src/lib/pages/frontsite/Literature/LiteratureSearch/SearchMessage/SearchMessage.js
+++ src/lib/pages/frontsite/Literature/LiteratureSearch/SearchMessage/SearchMessage.js
@@ -30,37 +30,33 @@ class SearchMessage extends Component {
       </Link>
     );
 
-    const EmptyResultOrangeMsg = () => {
-      return (
-        <Message icon color="orange" size="small">
-          <Icon name="info circle" />
-          <Overridable id="SearchResults.NoResults.extra">
-            <Message.Content>
-              <h4 className="search-no-results-extra">
-                Please fill in {requestFormLink} to request new additions or
-                purchases to the catalogue. (Login required)
-              </h4>
-            </Message.Content>
-          </Overridable>
-        </Message>
-      );
-    };
-    const ResultBlueMsg = () => {
-      return (
-        <Message icon info>
-          <Icon name="info circle" />
-          <Overridable id="SearchResults.NoResults.extra">
-            <Message.Content>
-              <Message.Header>
-                Couldn't find the literature you were looking for?
-              </Message.Header>
+    const EmptyResultOrangeMsg = () => (
+      <Message icon color="orange" size="small">
+        <Icon name="info circle" />
+        <Overridable id="SearchResults.NoResults.extra">
+          <Message.Content>
+            <h4 className="search-no-results-extra">
               Please fill in {requestFormLink} to request new additions or
               purchases to the catalogue. (Login required)
-            </Message.Content>
-          </Overridable>
-        </Message>
-      );
-    };
+            </h4>
+          </Message.Content>
+        </Overridable>
+      </Message>
+    );
+    const ResultBlueMsg = () => (
+      <Message icon info>
+        <Icon name="info circle" />
+        <Overridable id="SearchResults.NoResults.extra">
+          <Message.Content>
+            <Message.Header>
+              Couldn't find the literature you were looking for?
+            </Message.Header>
+            Please fill in {requestFormLink} to request new additions or
+            purchases to the catalogue. (Login required)
+          </Message.Content>
+        </Overridable>
+      </Message>
+    );
 
     return totalResults > 0 ? <ResultBlueMsg /> : <EmptyResultOrangeMsg />;
   }
