0218fd181a8b82bf678d9dc02baeb3b8038820fb
Saksham <sakshamarora1001@gmail.com>
2024-09-02 15:54:25 +0200

    authentication: pages: Add banner to login page

--- src/lib/authentication/pages/Login/Login.js
+++ src/lib/authentication/pages/Login/Login.js
@@ -1,6 +1,7 @@
 import { parseParams } from '@authentication/utils';
 import { EnvironmentLabel } from '@components/EnvironmentLabel';
 import { Media } from '@components/Media';
+import { Banner } from '@components/Banner';
 import { Notifications } from '@components/Notifications';
 import { invenioConfig } from '@config';
 import { goTo } from '@history';
@@ -245,12 +246,15 @@ class Login extends Component {
     const { hasError, errorHeader, errorMessage } = this.state;
     this.redirectIfAlreadyLoggedIn();
     return (
-      <LoginLayout
-        hasError={hasError}
-        errorHeader={errorHeader}
-        errorMessage={errorMessage}
-        {...this.props}
-      />
+      <>
+        <Banner />
+        <LoginLayout
+          hasError={hasError}
+          errorHeader={errorHeader}
+          errorMessage={errorMessage}
+          {...this.props}
+        />
+      </>
     );
   }
 }
