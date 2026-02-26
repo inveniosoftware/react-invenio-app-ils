c52372324bca0a19c974b6d69ba271bf2090c682
jakob miesner <jakob.miesner@gmail.com>
2026-02-26 11:53:46 +0100

    tests: update tests to work on new react and jest version
    * added list of expected warnings during test
    * made use of async tests
    * wrapped tests in `act()` that require it

--- src/lib/pages/backoffice/Document/DocumentDetails/DocumentStats/state/actions.test.js
+++ src/lib/pages/backoffice/Document/DocumentDetails/DocumentStats/state/actions.test.js
@@ -28,51 +28,45 @@ beforeEach(() => {
 });
 
 describe('Document stats fetch tests', () => {
-  it('should dispatch a loading action when fetching document stats', () => {
+  it('should dispatch a loading action when fetching document stats', async () => {
     const expectedAction = {
       type: actions.IS_LOADING,
     };
-    store.dispatch(actions.fetchDocumentStats(ARGS)).then((done) => {
-      const range = encodeURI(`{${ARGS.fromDate} TO ${ARGS.toDate}}`);
-      expect(mockLoanList).toHaveBeenCalledWith(
-        `document_pid:${ARGS.documentPid} AND state:(ITEM_RETURNED OR CANCELLED) AND start_date:${range}`
-      );
-      expect(store.getActions()[0]).toEqual(expectedAction);
-      done();
-    });
+    await store.dispatch(actions.fetchDocumentStats(ARGS));
+    const range = encodeURI(`{${ARGS.fromDate} TO ${ARGS.toDate}}`);
+    expect(mockLoanList).toHaveBeenCalledWith(
+      `document_pid:${ARGS.documentPid} AND state:(ITEM_RETURNED OR CANCELLED) AND start_date:${range}`
+    );
+    expect(store.getActions()[0]).toEqual(expectedAction);
   });
 
-  it('should dispatch a success action when document stats fetch succeeds', () => {
+  it('should dispatch a success action when document stats fetch succeeds', async () => {
     mockLoanList.mockResolvedValue({ data: 'someData' });
     const expectedAction = {
       type: actions.SUCCESS,
       payload: 'someData',
     };
 
-    store.dispatch(actions.fetchDocumentStats(ARGS)).then((done) => {
-      const range = encodeURI(`{${ARGS.fromDate} TO ${ARGS.toDate}}`);
-      expect(mockLoanList).toHaveBeenCalledWith(
-        `document_pid:${ARGS.documentPid} AND state:(ITEM_RETURNED OR CANCELLED) AND start_date:${range}`
-      );
-      expect(store.getActions()[1]).toEqual(expectedAction);
-      done();
-    });
+    await store.dispatch(actions.fetchDocumentStats(ARGS));
+    const range = encodeURI(`{${ARGS.fromDate} TO ${ARGS.toDate}}`);
+    expect(mockLoanList).toHaveBeenCalledWith(
+      `document_pid:${ARGS.documentPid} AND state:(ITEM_RETURNED OR CANCELLED) AND start_date:${range}`
+    );
+    expect(store.getActions()[1]).toEqual(expectedAction);
   });
 
-  it('should dispatch an error action when document stats fetch fails', () => {
+  it('should dispatch an error action when document stats fetch fails', async () => {
     mockLoanList.mockRejectedValue([500, 'Error']);
     const expectedAction = {
       type: actions.HAS_ERROR,
       payload: [500, 'Error'],
     };
 
-    store.dispatch(actions.fetchDocumentStats(ARGS)).then((done) => {
-      const range = encodeURI(`{${ARGS.fromDate} TO ${ARGS.toDate}}`);
-      expect(mockLoanList).toHaveBeenCalledWith(
-        `document_pid:${ARGS.documentPid} AND state:(ITEM_RETURNED OR CANCELLED) AND start_date:${range}`
-      );
-      expect(store.getActions()[1]).toEqual(expectedAction);
-      done();
-    });
+    await store.dispatch(actions.fetchDocumentStats(ARGS));
+    const range = encodeURI(`{${ARGS.fromDate} TO ${ARGS.toDate}}`);
+    expect(mockLoanList).toHaveBeenCalledWith(
+      `document_pid:${ARGS.documentPid} AND state:(ITEM_RETURNED OR CANCELLED) AND start_date:${range}`
+    );
+    expect(store.getActions()[1]).toEqual(expectedAction);
   });
 });
